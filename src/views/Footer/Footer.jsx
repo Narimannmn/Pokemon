acec280d9d62a8a43ccdc0de6a9680970142e4d4
Narimannmn <138319759+Narimannmn@users.noreply.github.com>
2024-05-22 00:07:07 +0500

    added footer view

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -3,7 +3,56 @@ import style from './Footer.module.css'
 function Footer() {
     return (
         <div className={style.footer}>
-            <img alt=""/>
+            <div className={style.footerSection}>
+                <h2>
+                    The Pokemon Company
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur atque deleniti dolorum
+                    eaque, est explicabo fuga harum hic impedit incidunt ipsam iusto, laudantium magnam magni
+                    molestiae natus nihil nisi nostrum officia perspiciatis quae quaerat quia quibusdam quisquam
+                    recusandae repellendus repudiandae rerum saepe sequi sint soluta tempora tempore tenetur totam
+                    ullam unde ut velit voluptatem.
+                </p>
+
+            </div>
+            <div className={style.footerSection}>
+                <h4>
+                    Our Social Media
+                </h4>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Youtube</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+            <div className={style.footerSection}>
+                <ul>
+
+                    <li>
+                        <a>Terms of Use</a>
+                    </li>
+
+                    <li>
+                        <a>Privacy Notice</a>
+                    </li>
+
+                    <li>
+                        <a>Cookie Page</a>
+                    </li>
+
+                    <li>
+                        <a>Legal Info</a>
+                    </li>
+
+                    <li>
+                        <a>Security</a>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     );
 }
